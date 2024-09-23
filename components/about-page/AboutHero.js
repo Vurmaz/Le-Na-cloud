5d0cc7b8c3bec5f4e2a1a52f77fa516ebfa0308c
Vurmaz <sadikvurmaz@gmail.com>
2024-09-23 17:46:29 +0300

    error fixed

--- components/about-page/AboutHero.js
+++ components/about-page/AboutHero.js
@@ -18,9 +18,9 @@ export default function AboutHero() {
     <div className=' h-auto text-white'>
         <div id='about-hero' className='flex flex-col gap-2 md:h-[65vh] h-max bg-[--primary] pt-8 pb-8 md:pb-0'>
             <h1 className='text-2xl md:text-4xl text-center w-full md:w-4/6 mx-auto'>Salesforce Mükemmelliği ile İşinizi Güçlendirin </h1>
-            <p className='text-sm w-5/6 md:w-1/2 mx-auto  text-center'>Sertifikalı uzmanlardan oluşan ekibimiz, müşteri ilişkilerinizi dönüştürmeye, operasyonları kolaylaştırmaya ve büyümeyi teşvik etmeye kendini adamıştır. Salesforce'un tüm potansiyelini ortaya çıkarmak ve işinizi ileriye taşımak için bizimle iş ortaklığı yapın.</p>
+            <p className='text-sm w-5/6 md:w-1/2 mx-auto text-center'>{`Sertifikalı uzmanlardan oluşan ekibimiz, müşteri ilişkilerinizi dönüştürmeye, operasyonları kolaylaştırmaya ve büyümeyi teşvik etmeye kendini adamıştır. Salesforce'un tüm potansiyelini ortaya çıkarmak ve işinizi ileriye taşımak için bizimle iş ortaklığı yapın.`}</p>
             <motion.button 
-            className='mt-6 bg-[--light] sm:text-base rounded-full text-[--text] shadow w-max h-max p-4 mx-auto whitespace-nowrap	'
+            className='mt-6 bg-[--light] sm:text-base rounded-full text-[--text] shadow w-max h-max p-4 mx-auto whitespace-nowrap'
             whileHover={{
                 backgroundColor:'var(--hovered)',
                 color:'var(--light)'
